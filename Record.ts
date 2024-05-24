0258e4271a0cb2eee30d8a805dd3af611a97516d
Alexander Carr <alex@gogbs.com>
2024-05-24 16:45:05 -0700

    initial commit

---
+++ Record.ts
@@ -0,0 +1,25 @@
+export interface Record {
+	"Name" : string
+	"Artwork File" : string
+	"Back Artwork File" : string
+	"Ordered" : string
+	"Width" : string
+	"Height" : string
+	"Bleed" : string
+	"Stock" : string
+	"Grain" : string
+	"grade" : string
+	"Max Overruns" : string
+	"Bleed Type" : string
+	"Spacing Type" : string
+	"Die Design Name" : string
+	"CAD File" : string
+	"Die Design Source" : string
+	"Priority" : string
+	"Description" : string
+	"Marks" : string
+	"Template" : string
+	"Sheet" : string
+	"MAC Artwork File" : string
+	"MAC Back Artwork File" : string
+}
---
+++ ReportProduct.ts
@@ -0,0 +1,5 @@
+export interface ReportProduct {
+	index: number
+	description: string
+	layouts: Array<{index: number, placed: number}>
+}
